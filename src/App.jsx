65b649f27077e982b3563fee1df6e3e7c3ea8a3c
margaretsoler <margaretamsoler@gmail.com>
2023-11-05 17:59:33 +0100

    :sparkles: logo in

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './index.css';
@@ -8,6 +6,8 @@ import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import logo from './assets/logogencives.png';
+
 
 
 
@@ -15,7 +15,7 @@ function ResponsiveAutoExample() {
   return (
     <Container>
       <Row>
-        <Col sm={8}>sm=8</Col>
+        <Col sm={8}><img src={logo} alt="logo" className='logo'/></Col>
         <Col sm={4}>sm=4</Col>
       </Row>
     </Container>
