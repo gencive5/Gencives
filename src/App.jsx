d9eda6d1b9ad67cc269a2643f6a5e8aec3754f43
margaretsoler <margaretamsoler@gmail.com>
2023-11-05 16:14:09 +0100

    🚀 Create project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function App() {
+  return (
+    <>
+      <Button>press me</Button>
+    </>
+  )
+}
+
+export default App
