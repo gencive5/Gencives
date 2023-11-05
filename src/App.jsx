a0e78bfe44c4acc3f5da680b266d59722568b6af
margaretsoler <margaretamsoler@gmail.com>
2023-11-05 17:31:35 +0100

    :alembic: bootstrap columns
    changed to white background ? :/

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function ResponsiveAutoExample() {
+  return (
+    <Container>
+      <Row>
+        <Col sm={8}>sm=8</Col>
+        <Col sm={4}>sm=4</Col>
+      </Row>
+    </Container>
+  );
+}
 
 function App() {
   return (
     <>
+      <ResponsiveAutoExample />
       <Button>press me</Button>
     </>
   )
 }
 
-export default App
+export default App;
+
