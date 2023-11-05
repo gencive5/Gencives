69c9313299ce09184eb1fc48cc8802821b957f8f
margaretsoler <margaretamsoler@gmail.com>
2023-11-05 17:41:29 +0100

    columns ok + added bootstrap css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './index.css';
 import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function ResponsiveAutoExample() {
