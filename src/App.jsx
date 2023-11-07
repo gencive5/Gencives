c2bf0562a05a70bd7c3d6ee4fbe59f5a8b3a0b4d
margaretsoler <margaretamsoler@gmail.com>
2023-11-07 15:57:10 +0100

    :clown_face: no more margins
    :'((((((

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,29 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './index.css';
-import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import logo from './assets/logogencives.png';
 
 
-
-
 function App() {
 
   
   return (
-    <Container-fluid>
+
+
+    <Container fluid={true} classname="custom-container">
       <Row>
-        <Col sm={8} className="p-0">
+        <Col sm={8}>
           <img src={logo} alt="logo" className='logo'/>
           <p>scroll</p>
         </Col>
-        <Col sm={4} className="p-0"><div className="touch-zone"></div></Col>
+        <Col sm={4}><div className="touch-zone"></div></Col>
       </Row>
-    </Container-fluid>
+    </Container>
   );
 }
-
 
 
 
