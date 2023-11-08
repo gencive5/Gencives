00629b87e45a2c0607129b652df6d2542126c8f7
margaretsoler <margaretamsoler@gmail.com>
2023-11-09 00:42:59 +0100

    changed gradient and no more scroll on 1st page
    the logo will be a btn to see the carousel

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Col sm={8} className="d-flex align-items-center justify-content-center">
           <div className="main-zone">
           <img src={logo} alt="logo" className='logo'/>
-          <p>scroll</p>
+          
           </div>
         </Col>
         <Col sm={4} className="d-none d-lg-block"><div className="touch-zone"></div></Col>
@@ -31,3 +31,4 @@ function App() {
 
 
 export default App;
+
