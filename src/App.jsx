fec24846242c97e90d4192e66c1065cbbed2b490
margaretsoler <margaretamsoler@gmail.com>
2023-11-10 17:47:36 +0100

    carousel with logo almost works
    only issue is that the logo is stretched

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './index.css';
@@ -9,66 +9,86 @@ import Carousel from 'react-bootstrap/Carousel';
 import logo from './assets/logogencives.png';
 
 function App() {
-  const [showCarousel, setShowCarousel] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handleLogoClick = () => {
-    setShowCarousel(!showCarousel);
     setActiveIndex(1);
   };
 
-  const handleCarouselSlide = (selectedIndex, e) => {
-    setActiveIndex(selectedIndex);
+  const handleCarouselSelect = (selectedIndex) => {
     if (selectedIndex === 5) {
-      // If the last slide is reached, go back to the first slide
-      setShowCarousel(false);
-      setTimeout(() => {
-        setActiveIndex(0);
-        setShowCarousel(true);
-      }, 0);
+      // Manually reset the active index to the first slide when transitioning from the last slide
+      setActiveIndex(0);
+    } else {
+      setActiveIndex(selectedIndex);
     }
   };
-
-  useEffect(() => {
-    // Automatically transition to the next slide every 3 seconds
-    const interval = setInterval(() => {
-      if (activeIndex < 5) {
-        setActiveIndex(activeIndex + 1);
-      } else {
-        setActiveIndex(0);
-      }
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [activeIndex]);
 
   return (
     <Container fluid={true} className="custom-container">
       <Row>
         <Col sm={8} className="d-flex align-items-center justify-content-center">
-          <Carousel
-            interval={null}
-            activeIndex={activeIndex}
-            onSelect={handleCarouselSlide}
-          >
-            <Carousel.Item>
-              <img
-                className={`d-block w-100 ${showCarousel ? '' : 'logo'}`}
-                src={logo}
-                alt="Logo"
-                onClick={handleLogoClick}
-              />
-            </Carousel.Item>
-            {[...Array(4)].map((_, index) => (
-              <Carousel.Item key={index + 1}>
+          {activeIndex === 0 ? (
+            // Display the logo with CSS when on the first slide
+            <img
+              className="d-block w-100 logo"
+              src={logo}
+              alt="Logo"
+              onClick={handleLogoClick}
+            />
+          ) : (
+            // Display the Carousel when on any slide other than the first
+            <Carousel
+              interval={null}
+              activeIndex={activeIndex}
+              onSelect={handleCarouselSelect}
+            >
+              {/* Second Slide */}
+              <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src={`https://placekitten.com/800/40${index + 1}`}
-                  alt={`Slide ${index + 1}`}
+                  src="https://placekitten.com/800/400"
+                  alt="Slide 1"
                 />
               </Carousel.Item>
-            ))}
-          </Carousel>
+
+              {/* Third Slide */}
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://placekitten.com/800/401"
+                  alt="Slide 2"
+                />
+              </Carousel.Item>
+
+              {/* Fourth Slide */}
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://placekitten.com/800/402"
+                  alt="Slide 3"
+                />
+              </Carousel.Item>
+
+              {/* Fifth Slide */}
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://placekitten.com/800/403"
+                  alt="Slide 4"
+                />
+              </Carousel.Item>
+
+              {/* Sixth Slide */}
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://placekitten.com/800/404"
+                  alt="Slide 5"
+                />
+              </Carousel.Item>
+            </Carousel>
+          )}
         </Col>
         <Col sm={4} className="d-none d-lg-block">
           <div className="touch-zone">
@@ -81,4 +101,6 @@ function App() {
 }
 
 export default App;
+
+
 
