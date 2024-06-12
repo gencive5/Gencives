cb0e13fbbcb1d3b6413ab263666fb41d6a4d38e4
margaretsoler <margaretamsoler@gmail.com>
2024-06-12 23:04:01 +0200

    new arrows

--- src/components/NavigationButtons.jsx
+++ src/components/NavigationButtons.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import FlecheHaut from '../assets/flechehaut.svg';
-import FlecheBas from '../assets/flechebas.svg';
+import ArrowTop from '../assets/arrowtop.svg';
+import ArrowBottom from '../assets/arrowbottom.svg';
 import '../App.css';
 
 
@@ -10,14 +10,14 @@ function NavigationButtons({ handlePrevious, handleNext }) {
       <button className="custom-btn" onClick={handlePrevious}>
         <div>
           <p>Previous</p>
-          <img style={{ height: 20 }} src={FlecheHaut} alt="Previous" />
+          <img style={{ height: 15 }} src={ArrowTop} alt="Previous" />
         </div>
       </button>
 
       <button className="custom-btn" onClick={handleNext}>
         <div>
           <p>Next</p>
-          <img style={{ height: 20 }} src={FlecheBas} alt="Next" />
+          <img style={{ height: 15 }} src={ArrowBottom} alt="Next" />
         </div>
       </button>
     </div>
