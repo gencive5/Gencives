fc1c6863f0e614645d988294de47e78a4c8fbdf0
gencive5 <vic.segen@gmail.com>
2025-02-04 23:38:55 +0100

    gif changes color

--- src/components/GifCarousel.jsx
+++ src/components/GifCarousel.jsx
@@ -3,10 +3,10 @@ import Carousel from 'react-bootstrap/Carousel';
 import gif1 from '../assets/gifs/gif1.gif';
 import gif2 from '../assets/gifs/gif2.gif';
 import gif3 from '../assets/gifs/gif3.gif';
-import gif4 from '../assets/gifs/gif1.gif';
-import gif5 from '../assets/gifs/gif2.gif';
-import gif6 from '../assets/gifs/gif3.gif';
-import gif7 from '../assets/gifs/gif2.gif';
+import gif4 from '../assets/gifs/gif4.gif';
+import gif5 from '../assets/gifs/gif5.gif';
+import gif6 from '../assets/gifs/gif6.gif';
+import gif7 from '../assets/gifs/gif7.gif';
 
 const gifSlides = [gif1, gif2, gif3, gif4, gif5, gif6, gif7];
 
